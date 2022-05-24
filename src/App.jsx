2523c5aac600f23c25d3cc0e2779d46bd12c2aca
Felix Macaspac <fmcmacaspac@protonmail.com>
2022-05-24 21:48:24 +0800

    💄 update CSS structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App bg-primary">
+      <h1 className='text-white text-7xl font-bold text-center flex min-h-screen items-center justify-center'>Vite + React + Tailwind</h1>
     </div>
   )
 }
