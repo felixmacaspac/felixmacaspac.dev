5985cef4075683effe9be76587fc093069b3c507
Felix Macaspac <fmcmacaspac@protonmail.com>
2022-07-10 20:29:56 +0800

    ✨ created loading state for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import TabSection from "./components/TabSection";
@@ -9,24 +10,58 @@ import "./index.css";
 import SectionWrapper from "./components/Global/SectionWrapper";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  window.addEventListener("load", (e) => {
+    setLoading(true);
+  });
+
   return (
-    <>
-      <NavBar />
-      <HeroSection />
-
-      <SectionWrapper id="about" heading="ABOUT ME">
-        <TabSection />
-      </SectionWrapper>
-
-      <SectionWrapper id="experience" heading="EXPERIENCE">
-        <TwoColumnCard />
-      </SectionWrapper>
-
-      <SectionWrapper id="projects" heading="PROJECTS">
-        <FeaturedAlternate />
-      </SectionWrapper>
-      <Footer />
-    </>
+    <div>
+      {loading ? (
+        <>
+          <NavBar />
+          <HeroSection />
+          <SectionWrapper id="about" heading="ABOUT ME">
+            <TabSection />
+          </SectionWrapper>
+          <SectionWrapper id="experience" heading="EXPERIENCE">
+            <TwoColumnCard />
+          </SectionWrapper>
+          <SectionWrapper id="projects" heading="PROJECTS">
+            <FeaturedAlternate />
+          </SectionWrapper>
+          <Footer />
+        </>
+      ) : (
+        <div className="loading-wrapper">
+          <div className="flex gap-8 items-center justify-center">
+            <span className="text-white font-display md:text-7xl">Loading</span>
+            <svg
+              className="animate-spin -ml-1 mr-3 h-16 w-16 text-secondary fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="current"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25 fill-current text-secondary-300"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+                fill="current"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
