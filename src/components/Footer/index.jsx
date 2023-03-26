bf1e6695724e233afb2f6a25bb10b80440e47d4b
Felix Macaspac <fmcmacaspac@protonmail.com>
2023-03-26 21:46:18 +0800

    ⚡️make copyright year dynamic
    - add GTM

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="bg-white py-16 dark:bg-primary">
       <div className="text-center">
         <p className="text-primary font-normal text-base mb-2 dark:text-white">
-          © 2022 Felix Macaspac. All rights reserved.
+          © {currentYear} Felix Macaspac. All rights reserved.
         </p>
         <p className="text-primary font-semibold text-base dark:text-white">
           Made with {"<3"}
